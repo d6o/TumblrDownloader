9d1b00399247a0f4fe8d7b3472660bddf9b046c9
Diego Siqueira <diego9889@gmail.com>
2016-01-16 12:42:29 -0200

    A lot of refactoring, class created, resolution and tag selectors created. Chronological order added

--- tumblrdownloader.py
+++ tumblrdownloader.py
@@ -11,96 +11,100 @@ import os
 import sys
 import argparse
 
+class TumblrDownloader:
 
-API_URL = "http://#subdomain#.tumblr.com/api/read?type=photo&num=#chunck#&start=#start#"
+	api_url	= 'http://#subdomain#.tumblr.com/api/read?type=photo&num=#chuck#&start=#start#' 
 
-def createfolder(name):
-	'''
-		if folder does not exist, create it.
-	'''
-	if not os.path.exists(name):
- 		os.makedirs(name)
+	def __init__(self, subdomain, chuck, output, resolution, tagged, chrono):
+		self._subdomain = subdomain
+		self._chuck = chuck
+		self._output = output
+		self._resolution = resolution
+		self._tagged = tagged
+		self._chrono = chrono
 
-def downloadimage(url,subdomain,output):
-	'''
-		Download a image to subdomain folder.
-	'''
-	file_name 	= subdomain + "_" + url.split('/')[-1]
-	folder_path = output + "/" + subdomain
+		self.api_url = self.api_url.replace("#subdomain#",self._subdomain)
+		self.api_url = self.api_url.replace("#chuck#",str(self._chuck))
 
-	createfolder(folder_path)
+		self._image_prefix = self._subdomain;
 
-	u 			= urllib2.urlopen(url)
-	f 			= open(folder_path + "/" + file_name, 'wb')
-	meta 		= u.info()
-	file_size 	= int(meta.getheaders("Content-Length")[0])
-	print "Downloading: %s Bytes: %s" % (file_name, file_size)
+		self._folder_path = self._output + "/" + self._subdomain + "/" + str(self._resolution)
 
-	file_size_dl 	= 0
-	block_sz		= 8192
-	while True:
-	    buffer = u.read(block_sz)
-	    if not buffer:
-	        break
+		if (self._chrono):
+			self.api_url += "&chrono=1"
 
-	    file_size_dl += len(buffer)
-	    f.write(buffer)
-	    status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-	    status = status + chr(8)*(len(status)+1)
-	    print status,
+		if self._tagged:
+			self.api_url 		+= "&tagged=" + self._tagged
+			self._folder_path 	+= "/" + self._tagged
+			self._image_prefix	+= "_" + self._tagged;
 
-	f.close()
+		self._createfolder()
 
-def getimages(site,start,chunck):
-	'''
-		Get all images returned by Tumblr API
-	'''
-	site = site.replace("#chunck#",str(chunck))
-	site = site.replace("#start#",str(start))
-	file = urllib2.urlopen(site)
-	data = file.read()
-	file.close()
-	regex		= ur"<photo-url max-width=\"1280\">(.+?)</photo-url>"
-	imagelist	= re.findall(regex, data)
-	return imagelist
+	def _createfolder(self):
+		'''
+			if folder does not exist, create it.
+		'''
 
-def finish():
-	'''
-		Finish message
-	'''
-	print 'All images were downloaded.'
-	sys.exit()
+		if not os.path.exists(self._folder_path):
+	 		os.makedirs(self._folder_path)
 
-def download(subdomain,chunck,output):
-	'''
-		download all images from a Tumblr
-	'''
-	site  = API_URL.replace("#subdomain#",subdomain)
-	start = 0
+	def download(self):
+		'''
+			download all images from a Tumblr
+		'''
 
-	while True:
-		imagelist = getimages(site,start,chunck)
-		start     = start + chunck
+		start = 0
 
-		if not imagelist:
-			finish()
+		while True:
+			imagelist = self._getimages(start)
+			start     = start + self._chuck
 
-		for image in imagelist:
-			downloadimage(image,subdomain,output)
+			if not imagelist:
+				break
 
-def help(error):
-	'''
-		Print help message
-	'''
-	if (error > 0):
-		print 'Error:', error
-		print ''
-	print 'tumblrdownloader.py -s <subdomain> -t <chunck> -o <output>'
-	print 'Tumblr: http://bibliammo.tumblr.com/'
-	print 'example: tumblrdownloader.py -s bibliammo'
-	print 'example: tumblrdownloader.py -s bibliammo -c 10'
-	print 'example: tumblrdownloader.py -s bibliammo -o allimages'
-	sys.exit(error)
+			for image in imagelist:
+				self._downloadimage(image)
+
+	def _getimages(self, start):
+		'''
+			Get all images returned by Tumblr API
+		'''
+		site = self.api_url.replace("#start#",str(start))
+
+		file = urllib2.urlopen(site)
+		data = file.read()
+		file.close()
+
+		regex		= ur"<photo-url max-width=\"" + str(self._resolution) + "\">(.+?)</photo-url>"
+		imagelist	= re.findall(regex, data)
+		return imagelist
+
+	def _downloadimage(self,url):
+		'''
+			Download a image to subdomain folder.
+		'''
+		file_name 	= self._image_prefix + "_" + url.split('/')[-1]
+
+		u 			= urllib2.urlopen(url)
+		f 			= open(self._folder_path + "/" + file_name, 'wb')
+		meta 		= u.info()
+		file_size 	= int(meta.getheaders("Content-Length")[0])
+		print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+		file_size_dl 	= 0
+		block_sz		= 8192
+		while True:
+		    buffer = u.read(block_sz)
+		    if not buffer:
+		        break
+
+		    file_size_dl += len(buffer)
+		    f.write(buffer)
+		    status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+		    status = status + chr(8)*(len(status)+1)
+		    print status,
+
+		f.close()
 
 def main(argv):
 	parser = argparse.ArgumentParser(description="Download all images from a Tumblr")
@@ -108,23 +112,27 @@ def main(argv):
 		help="Tumblr subdomain you want to download")
 	parser.add_argument("--chuck", type=int, default=50, 
 		help="The number of posts to return each call to Tumblrs API")
-	parser.add_argument("--chrono", action="store_true", 
-		help="Sort in chronological order (oldest first)")
-	parser.add_argument("--resolution", type=int, default=1280, choices=[1280, 500, 400, 250, 100, 75],
-        help="Select Max Width to download")
 	parser.add_argument("--output", type=str, default="images", 
 		help="Output folder")
+	parser.add_argument("--resolution", type=int, default=1280, choices=[1280, 500, 400, 250, 100, 75],
+        help="Select Max Width to download")
 	parser.add_argument("--tagged", type=str,
 		help="Download only images with tag")
+	parser.add_argument("--chrono", action="store_true", 
+		help="Sort in chronological order (oldest first)")
 
 	args = parser.parse_args()
 
 	print 'Downloading Subdomain: ', args.subdomain
 
 	try:
-		download(args.subdomain,args.chuck,args.output)
+		td = TumblrDownloader(args.subdomain,args.chuck,args.output,args.resolution,args.tagged,args.chrono)
+		td.download()
+		print 'All images were downloaded.'
 	except KeyboardInterrupt:
 		print 'Interrupt received, stopping downloads'
+
+	sys.exit()
 
 if __name__ == "__main__":
    main(sys.argv[1:])
